2889dec9f056b45627809ed402617150d2977aec
redsir12 <158326080+redsir12@users.noreply.github.com>
2024-02-20 22:47:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['917909167192', 'Red', true],
+  ['917909167192', 'REDSIR', true], 
   [''] 
 ] //Number of owners
 
